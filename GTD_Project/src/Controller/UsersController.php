096e3c84437645ea1526d95c0f9ca12ffff826d2
Girard <paul.girard1@viacesi.fr>
2020-11-12 15:53:19 +0100

    [Add] Profile Gestion

--- GTD_Project/src/Controller/UsersController.php
+++ GTD_Project/src/Controller/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Comments;
 use App\Entity\Users;
 use App\Form\RegisterType;
 use App\Repository\UsersRepository;
@@ -9,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Security;
@@ -39,7 +41,7 @@ class UsersController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute("genre_index");
+            return $this->redirectToRoute("login");
         }
 
         return $this->render('users/register.html.twig', [
@@ -65,11 +67,11 @@ class UsersController extends AbstractController
      */
     public function logout()
     {
-        return $this->render('genre/index.html.twig');
+        return $this->redirectToRoute("register");
     }
 
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/profile", name="profile", methods={"GET", "POST"})
      * @param Security $security
      * @param UsersRepository $usersRepository
      * @return Response
@@ -79,6 +81,65 @@ class UsersController extends AbstractController
         return $this->render('users/profile.html.twig', [
             "user" => $security->getUser()
         ]);
+    }
 
+    /**
+     * @Route("/profile/{id}", name="edit_profile", methods={"GET", "POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param UserPasswordEncoderInterface $encoder
+     * @param Security $security
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function editProfile(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder, Security $security)
+    {
+        $utilisateur = $security->getUser();
+        $form = $this->createForm(RegisterType::class, $utilisateur);
+        $form->handleRequest($request);
+
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $passwordEncrypt = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordEncrypt);
+            $utilisateur->setRoles("ROLE_USER");
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+            return $this->redirectToRoute("profile");
+        }
+
+        return $this->render('users/edit_profile.html.twig', [
+            "form" => $form->createView(),
+            "utilisateur" => $utilisateur
+
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="delete_profile", methods={"DELETE"})
+     * @param Request $request
+     * @param Session $session
+     * @param Security $security
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteProfile(Request $request, Session $session, Security $security)
+    {
+
+        if ($this->isCsrfTokenValid('delete'.$security->getUser()->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $currentUserId = $this->getUser()->getId();
+            if ($currentUserId == $security->getUser()->getId())
+            {
+                $session = $this->get('session');
+                $session = new Session();
+                $session->invalidate();
+            }
+            $entityManager->remove($security->getUser());
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute("register");
     }
 }
